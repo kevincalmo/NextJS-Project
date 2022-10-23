9e3b1b00484c2b754323c50dcdac008b099814f9
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-10-23 15:40:02 -0400

    use my post api to send data to mongodb

--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,12 +1,29 @@
 //our-domain.com/new-meetup
 
+import { useRouter } from 'next/router'
 import React from 'react'
 import NewMeetUpForm from '../../components/meetups/NewMeetupForm'
 
 const NewMeetUpPage = () => {
 
-    const addMeetupHandler = (enteredMeetupdata) => {
-        console.log(enteredMeetupdata);
+    const router = useRouter();
+
+    const addMeetupHandler = async (enteredMeetupdata) => {
+        const response = await fetch('/api/new-meetup', {
+            /* Je fais une requete en POST */
+            method: 'POST',
+            /* Converti mon objet en json */
+            body: JSON.stringify(enteredMeetupdata),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        router.push('/');
     }
 
     return (
