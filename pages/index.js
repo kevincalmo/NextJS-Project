816996c5e71400740af9c6968d72d980dd6a29cc
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-10-23 09:50:41 -0400

    get data with the function getStaticprops

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,47 @@
 import React from 'react'
 import MeetupList from '../components/meetups/MeetupList';
 
-const HomePage = () => {
-    const DUMMY_MEET_UP = [
-        {
-            id: 'm1',
-            title: 'A first meetup',
-            image: 'https://picsum.photos/200/300',
-            address: 'Some address',
-            description: 'This is a first meetup'
-        },
-        {
-            id: 'm2',
-            title: 'A second meetup',
-            image: 'https://picsum.photos/200/300',
-            address: 'Some address',
-            description: 'This is a second meetup'
-        },
-        {
-            id: 'm3',
-            title: 'A thirdmeetup',
-            image: 'https://picsum.photos/200/300',
-            address: 'Some address',
-            description: 'This is a third meetup'
-        },
-    ];
+const DUMMY_MEET_UP = [
+    {
+        id: 'm1',
+        title: 'A first meetup',
+        image: 'https://picsum.photos/200/300',
+        address: 'Some address',
+        description: 'This is a first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'A second meetup',
+        image: 'https://picsum.photos/200/300',
+        address: 'Some address',
+        description: 'This is a second meetup'
+    },
+    {
+        id: 'm3',
+        title: 'A thirdmeetup',
+        image: 'https://picsum.photos/200/300',
+        address: 'Some address',
+        description: 'This is a third meetup'
+    },
+];
+
+const HomePage = (props) => {
+
     return (
         <>
-            <MeetupList meetups={DUMMY_MEET_UP} />
+            {props.meetups && <MeetupList meetups={props.meetups} />}
+
         </>
     )
+}
+
+export async function getStaticProps() {
+    // Send a http request and fetch data
+    return {
+        props: {
+            meetups: DUMMY_MEET_UP
+        }
+    }
 }
 
 export default HomePage;
