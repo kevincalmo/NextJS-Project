5718ea48002a00940c4ee9b61161e878bc21428e
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-10-22 22:20:53 -0400

    add form to create new meet up

--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,1 +1,20 @@
 //our-domain.com/new-meetup
+
+import React from 'react'
+import NewMeetUpForm from '../../components/meetups/NewMeetupForm'
+
+const NewMeetUpPage = () => {
+
+    const addMeetupHandler = (enteredMeetupdata) => {
+        console.log(enteredMeetupdata);
+    }
+
+    return (
+        <>
+            <NewMeetUpForm onAddMeetup={addMeetupHandler} />
+        </>
+
+    )
+}
+
+export default NewMeetUpPage;
